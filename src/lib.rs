716b2c05f85f6cc55bd43a02512d49d9cf7b73cb
Dzmitry Ramaniuk <kanumowa@gmail.com>
2021-08-08 22:41:56 +0200

    add tests

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+pub fn clsx_h(hash: HashMap<&str, bool>) -> String {
+    let mut str = "".to_owned();
+    for (key, value) in &hash {
+        if *value {
+            str.push_str(" ");
+            str.push_str(key);
+        }
+    }
+    str.trim().to_string()
+}
+
+pub fn clsx(arr: Box<[(&str, bool)]>) -> String {
+    let mut str = "".to_owned();
+    for (key, value) in arr.iter() {
+        if *value {
+            str.push_str(" ");
+            str.push_str(key);
+        }
+    }
+    str.trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::{clsx, clsx_h};
+    #[test]
+    fn clsx_array() {
+        assert_eq!(clsx(Box::new([("class1", true), ("class2", false), ("class3", true)])), "class1 class3");
+    }
+    #[test]
+    fn clsx_hash() {
+        let mut class_names = HashMap::new();
+        class_names.insert("key", true);
+        class_names.insert("key1", true);
+        class_names.insert("key2", false);
+        class_names.insert("key3", true);
+        assert_eq!(clsx_h(class_names), "key key1 key3");
+    }
+}
