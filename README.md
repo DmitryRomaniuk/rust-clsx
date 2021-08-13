1efd3f1cce822618111ab704966059ba1165d02f
Dima Ramaniuk <kanumowa@gmail.com>
2021-08-13 19:38:51 +0200

    remove extra code

--- README.md
+++ README.md
@@ -5,17 +5,5 @@ portable js library [clsx](https://github.com/lukeed/clsx/blob/master/src/index.
 ## How to use in yew framework
 
 ```rust
-use std::collections::HashMap;
-
 <i class=clsx(vec![("fa", true), ("fa-video-camera", self.state.enable_camera), ("fa-video-slash background__red", !self.state.enable_camera)]) />
-
-// or alternatrive
-
-let mut classes_mic = HashMap::new();
-classes_mic.insert("fa", true);
-classes_mic.insert("fa-microphone", self.state.enable_mic);
-classes_mic.insert("fa-microphone-slash background__red", !self.state.enable_mic);
-
-<i class=clsx_h(classes_mic) />
-
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,3 @@
-use std::collections::HashMap;
-
-pub fn clsx_h(hash: HashMap<&str, bool>) -> String {
-    let mut str = "".to_owned();
-    for (key, value) in &hash {
-        if *value {
-            str.push_str(&' '.to_string());
-            str.push_str(key);
-        }
-    }
-    str.trim().to_string()
-}
-
 pub fn clsx(arr: Vec<(&str, bool)>) -> String {
     let mut str = "".to_owned();
     for (key, value) in arr.iter() {
@@ -24,19 +11,9 @@ pub fn clsx(arr: Vec<(&str, bool)>) -> String {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-    use super::{clsx, clsx_h};
+    use super::{clsx};
     #[test]
     fn clsx_array() {
         assert_eq!(clsx(vec![("class1", true), ("class2", false), ("class3", true)]), "class1 class3");
-    }
-    #[test]
-    fn clsx_hash() {
-        let mut class_names = HashMap::new();
-        class_names.insert("key", true);
-        class_names.insert("key1", true);
-        class_names.insert("key2", false);
-        class_names.insert("key3", true);
-        assert_eq!(clsx_h(class_names), "key key1 key3");
     }
 }
